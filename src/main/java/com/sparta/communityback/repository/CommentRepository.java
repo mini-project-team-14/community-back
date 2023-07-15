c725585b2014950d7924252de3bef511ae884742
kss <kos7114@naver.com>
2023-07-15 16:18:50 +0900

    feat: post, comment

--- src/main/java/com/sparta/communityback/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.communityback.repository;
-
-import com.sparta.communityback.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
